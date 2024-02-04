943a4c32af971efedd61bddc774790c403bfba22
Jonalyn Ulloa <jculloa@myseneca.ca>
2024-02-04 03:47:11 +0000

    updated main and output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,10 @@
 # Step 10 - Add output variables
+
 output "public_ip" {
   value = aws_instance.clo835-host-EC2.public_ip
 }
 
+
 output "clo835_ecr" {
-    value = aws_ecr_repository.clo835-ecr.name
+  value = aws_ecr_repository.clo835-ecr.name
 }
